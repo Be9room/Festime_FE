e376d0a4792396a317d4d8a639e3daa39c391173
DongRyeong Lee <dr11291129@gmail.com>
2024-09-19 15:38:24 +0900

    chore: change meshgradient color
    - 비룡제 포스터 색상을 바탕으로 color값을 수정했습니다.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
 import MeshGradient from "../../components/MeshGradient";
 
 const Home = () => {
-  return <MeshGradient amount={5} lifetime={8000} primaryColor={"#3d1053"} secondaryColor={"#0a213f"} />;
+  return <MeshGradient amount={5} lifetime={8000} primaryColor={"#065147"} secondaryColor={"#0a3962"} />;
 };
 
 export default Home;
+
+/*
+<MeshGradient amount={5} lifetime={8000} primaryColor={"#3d1053"} secondaryColor={"#0a213f"} />
+*/
