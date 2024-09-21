77eb7e70359d615bd9275bdf7c427033e0b7e98d
DongRyeong Lee <dr11291129@gmail.com>
2024-09-21 15:06:13 +0900

    feat: add svgs and webp file
    - png를 대체할 webp 파일과 svg 파일을 추가했습니다.
    - main 이미지를 webp로 대체했습니다.
    - header component를 일부 수정했습니다.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,15 @@
+import MenuSVG from "../../assets/svgs/menu.svg?react";
+
 const Header = () => {
   return (
-    <header className="bold-14 flex max-w-2xl flex-row justify-between p-5 text-text-primary">
+    <header className="bold-14 fixed left-1/2 top-0 flex w-full max-w-2xl -translate-x-1/2 transform flex-row justify-between p-5 text-text-primary">
       <button className="z-50 flex h-10 flex-row items-center gap-3 rounded-[14px] border border-solid border-grayscale-gray-30 bg-grayscale-gray-20 p-3 backdrop-blur-2xl">
         <span>飛 龍 祭</span>
         <div className="h-6 w-[0.0625rem] bg-grayscale-gray-30" />
         <span className="rounded-lg bg-grayscale-gray-20 px-2 py-0.5">Home</span>
       </button>
       <button className="z-50 flex h-10 flex-row items-center gap-3 rounded-[14px] border border-solid border-grayscale-gray-30 bg-grayscale-gray-20 p-3 backdrop-blur-2xl">
-        <span>menu</span>
+        <MenuSVG />
       </button>
     </header>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,15 @@
 import Header from "../../components/Header";
 import MeshGradient from "../../components/MeshGradient";
-import main from "../../assets/images/main.png";
+import mainWebp from "../../assets/images/main.webp";
 
 const Home = () => {
   return (
     <>
       <MeshGradient amount={5} lifetime={8000} primaryColor={"#065147"} secondaryColor={"#0a3962"} />
       <Header />
-      <img className="w-screen z-[100] p-8" src={main} />
+      <img className="z-[100] w-screen p-8" src={mainWebp} />
     </>
   );
 };
 
 export default Home;
-
-/*
-<MeshGradient amount={5} lifetime={8000} primaryColor={"#3d1053"} secondaryColor={"#0a213f"} />
-*/
