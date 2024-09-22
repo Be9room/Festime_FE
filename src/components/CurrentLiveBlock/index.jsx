11bfe769e565672541d4d01948b1ef273a87c567
DongRyeong Lee <dr11291129@gmail.com>
2024-09-22 20:16:21 +0900

    feat: add darkgrayblock class at global.css
    - 동일한 코드 중복 작성 방지를 위해 darkgrayblock와 관련된 tailwind
      코드를 global.css에 추가했습니다.

--- src/components/CurrentLiveBlock/index.jsx
+++ src/components/CurrentLiveBlock/index.jsx
@@ -1,6 +1,6 @@
 const CurrentLiveBlock = () => {
   return (
-    <div className="flex w-full flex-col gap-3 rounded-[14px] border border-solid border-grayscale-darkgray-20 bg-grayscale-darkgray-20 p-4">
+    <div className="darkgrayblock darkgrayblock-block-hover flex w-full flex-col gap-3 p-4">
       <div className="medium-14 flex w-full flex-row items-center gap-3">
         <span className="text-live-red">LIVE</span>
         <hr className="h-0.5 flex-1 border-none bg-gradient-to-r from-[#FF3B3B] via-[#BBBBBB80] to-[#BBBBBB00]" />
