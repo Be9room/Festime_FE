46e9feeea2a8bd30b491bf55b08236326c9b4f50
DongRyeong Lee <dr11291129@gmail.com>
2024-09-22 16:55:04 +0900

    feat: add QuickLink component
    - QuickLink component를 추가했습니다.

--- src/pages/Home/components/QuickLink.jsx
+++ src/pages/Home/components/QuickLink.jsx
@@ -1,9 +1,13 @@
+import LinkBlock from "../../../components/LinkBlock";
 import TitleBlock from "../../../components/TitleBlock";
 
-const QuickLink = () => {
+const QuickLink = ({ linkArray }) => {
   return (
     <>
       <TitleBlock title="QUICK LINK" subtitle="자주 찾는 페이지들을 모아봤어요." />
+      <div className="flex w-full flex-col gap-3">
+        {linkArray && linkArray.map(({ text, to }) => <LinkBlock key={text} text={text} to={to} />)}
+      </div>
     </>
   );
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,16 +4,20 @@ import ArrowDown from "../../assets/svgs/arrowDown.svg?react";
 import CurrentTimetable from "./components/currentTimetable";
 import QuickLink from "./components/QuickLink";
 
+const LINK_ARRAY = [
+  { text: "예시 텍스트", to: "/" },
+  { text: "예시 텍스트", to: "/" },
+  { text: "예시 텍스트", to: "/" }
+];
+Object.freeze(LINK_ARRAY);
+
 const Home = () => {
   return (
     <Layout>
       <img className="h-[90dvh] w-full object-contain px-2 pb-8" src={mainWebp} />
-
       <ArrowDown className="my-8 w-full" />
-
       <CurrentTimetable />
-
-      <QuickLink />
+      <QuickLink linkArray={LINK_ARRAY} />
     </Layout>
   );
 };
