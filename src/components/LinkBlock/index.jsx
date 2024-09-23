a1ce2a401600daf14bfc05023a7dc9211a5dfd9d
DongRyeong Lee <dr11291129@gmail.com>
2024-09-23 22:59:20 +0900

    feat: edit LinkBlock onClick method
    - onClick 메서드 동작 방식을 변경했습니다.
    - mailto:, tel: 의 경우를 대응할 수 있도록 변경했습니다.
    - 페이지 공유 기능을 추가했습니다.

--- src/components/LinkBlock/index.jsx
+++ src/components/LinkBlock/index.jsx
@@ -1,12 +1,33 @@
 import CircleIcon from "../../assets/svgs/circle.svg?react";
 import { useNavigate } from "react-router-dom";
 
+const TEL_REGEXP = /^tel:\+82\d{1,2}\d{3,4}\d{4}$/;
+const MAIL_REGEXP = /^mailto:[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const SHARE_REGEXP = /^share$/;
+
 const LinkBlock = ({ text, className, to = "/" }) => {
   const navigate = useNavigate();
 
+  const handleButtonClicked = (to) => {
+    if (TEL_REGEXP.test(to) || MAIL_REGEXP.test(to)) {
+      window.location.href = to;
+    } else if (SHARE_REGEXP.test(to)) {
+      if (navigator.share) {
+        navigator
+          .share({
+            title: "Share this content",
+            url: window.location.href
+          })
+          .catch((error) => {
+            console.error("Error sharing:", error);
+          });
+      } else alert("Sharing is not supported in this browser.");
+    } else navigate(to);
+  };
+
   return (
     <button
-      onClick={() => navigate(to)}
+      onClick={() => handleButtonClicked(to)}
       className={`darkgrayblock darkgrayblock-button-hover flex w-full flex-row justify-between gap-3 p-4 ${className} items-center`}
     >
       <span className="medium-14 flex-1 text-left">{text}</span>
