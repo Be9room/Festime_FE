47a96366b021cb9d9a505b614cc0c0857b85085b
DongRyeong Lee <dr11291129@gmail.com>
2024-09-25 19:34:53 +0900

    feat: add all page template
    - 페이지 정보 업데이트 전 각 템플릿을 추가했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,13 @@ import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
 
 import Home from "./pages/Home";
+import Entry from "./pages/Entry/index";
 import Ticketing from "./pages/Ticketing";
 import TimeTable from "./pages/TimeTable";
+import Booths from "./pages/Booths/index";
+import Food from "./pages/Food/index";
+import Event from "./pages/Event/index";
+import Support from "./pages/Support/index";
 import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
@@ -18,8 +23,13 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/entry" element={<Entry />} />
           <Route path="/ticketing" element={<Ticketing />} />
           <Route path="/timetable" element={<TimeTable />} />
+          <Route path="/booths" element={<Booths />} />
+          <Route path="/food" element={<Food />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/support" element={<Support />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
         <Footer />
---
+++ src/pages/Support/index.jsx
@@ -0,0 +1,18 @@
+import Layout from "../../components/Layout";
+import TitleBlock from "../../components/TitleBlock";
+import LinkBlock from "./../../components/LinkBlock/index";
+
+const Support = () => {
+  return (
+    <Layout>
+      <TitleBlock title="총학생회 본부 부스 & 의무실 운영 안내" subtitle={["향후 업데이트 예정입니다."]} />
+
+      <TitleBlock className="mt-8" title="문의사항" />
+      <LinkBlock text="2024 비룡제 축제 이메일" to="mailto:inhasg.festival.2024@gmail.com" />
+      <LinkBlock className="mt-3" text="인하대학교 총학생회" to="tel:+82328608451" />
+      <LinkBlock className="mt-3" text="현재 페이지 공유하기" to="share" />
+    </Layout>
+  );
+};
+
+export default Support;
