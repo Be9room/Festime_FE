d1139873c3990113401271de6ffb901d2accbc27
DongRyeong Lee <dr11291129@gmail.com>
2024-09-26 00:17:26 +0900

    feat: update Food page information
    - Food 페이지의 정보를 업데이트 했습니다.

---
+++ src/constants/foodArray.js
@@ -0,0 +1,20 @@
+const FOOD_ARRAY = [
+  { name: "리얼키친", foodList: "불초밥, 새우갈릭초밥" },
+  { name: "에임키친", foodList: "수제 닭강정" },
+  { name: "스타케밥", foodList: "치킨케밥, 양고기케밥, 터키아이스크림" },
+  { name: "띠아모", foodList: "츄러스, 아이스크림 츄러스, 아이스크림" },
+  { name: "판타스틱붐", foodList: "타코야끼" },
+  { name: "킹오브더킹", foodList: "닭꼬치" },
+  { name: "수비드연구소", foodList: "큐브스테이크, 스테이크 덮밥" },
+  { name: "쉬림프쥬", foodList: "갈릭버터쉬림프, 타르치즈쉬림프, 새우꼬치, 갈릭버터쉬림프덮밥" },
+  { name: "황금마차", foodList: "야끼소바, 야끼소바빵" },
+  { name: "와플정류장", foodList: "홍콩에그와플, 소프트 아이스크림" },
+  { name: "만수무강", foodList: "팟타이" },
+  { name: "스몰여덟감자", foodList: "네덜란드감자튀김, 뿌링치즈볼" },
+  { name: "서프라이즈데이", foodList: "치즈폭탄마르게리타, 잠봉블랙페퍼" },
+  { name: "교촌쿡", foodList: "회오리감자, 호롱낙지, 문어꼬치" },
+  { name: "천사날개트럭", foodList: "아이스크림 크레페, 생크림 크레페, 솜사탕" }
+];
+Object.freeze(FOOD_ARRAY);
+
+export default FOOD_ARRAY;
--- src/pages/Food/index.jsx
+++ src/pages/Food/index.jsx
@@ -1,13 +1,79 @@
 import Layout from "../../components/Layout";
 import TitleBlock from "../../components/TitleBlock";
 import LinkBlock from "./../../components/LinkBlock/index";
+import Divider from "../../components/Divider";
+import FOOD_ARRAY from "./../../constants/foodArray";
 
 const Food = () => {
   return (
     <Layout>
-      <TitleBlock title="F&B존, 취식 구역 안내" subtitle={["향후 업데이트 예정입니다."]} />
+      <TitleBlock
+        title="축제 안내사항"
+        subtitle={[
+          "현 페이지는 아래 내용을 포함하고 있습니다.",
+          "• F&B존 및 취식 구역 운영 안내",
+          "• 푸드트럭 운영 리스트",
+          "• 이용 시 주의사항"
+        ]}
+      />
 
-      <TitleBlock className="mt-8" title="문의사항" />
+      <Divider />
+
+      <TitleBlock
+        title="F&B존 및 취식 구역 운영 안내"
+        subtitle={[
+          <span key="title" className="bold-14">
+            • 위치
+          </span>,
+          "정석학술정보관 뒤",
+          <br key="br" />,
+          <span key="title" className="bold-14">
+            • 취식 구역
+          </span>,
+          "정석학술정보관 뒤편 : 약 400석",
+          "본관 뒤편 : 약 600석",
+          <br key="br" />,
+          <span key="title" className="bold-14">
+            • 운영 시간
+          </span>,
+          "11:00 ~ 23:00"
+        ]}
+      />
+
+      <Divider />
+
+      <TitleBlock title="푸드트럭 운영 리스트" />
+      <div className="flex w-full flex-col gap-3">
+        {FOOD_ARRAY &&
+          FOOD_ARRAY.map(({ name, foodList }) => (
+            <div key="name" className="darkgrayblock darkgrayblock-block-hover flex w-full flex-col gap-2 p-4">
+              <p className="bold-16 break-keep">• {name}</p>
+              <p className="medium-14 w-full break-keep">{foodList}</p>
+            </div>
+          ))}
+      </div>
+
+      <Divider />
+
+      <TitleBlock
+        title="이용 시 주의사항"
+        subtitle={[
+          <span key="note" className="text-live-red">
+            ※ 사용한 일회용 용기와 쓰레기는 지정된 쓰레기통에 분리수거 후 폐기해주시기 바랍니다.
+          </span>,
+          <span key="note" className="text-live-red">
+            ※ F&B존 이용 및 대기 시 안전에 유의하여 이동해주시기 바랍니다. 특히 음식을 들고 이동하실 때는 넘어지거나
+            부딪히지 않도록 각별히 주의해 주시기 바랍니다.
+          </span>,
+          <span key="note" className="text-live-red">
+            ※ 부스에 따라 성인 인증을 요구할 수 있으니 신분증을 지참해 주시기 바랍니다.
+          </span>
+        ]}
+      />
+
+      <Divider />
+
+      <TitleBlock title="문의사항" />
       <LinkBlock text="2024 비룡제 축제 이메일" to="mailto:inhasg.festival.2024@gmail.com" />
       <LinkBlock className="mt-3" text="인하대학교 총학생회" to="tel:+82328608451" />
       <LinkBlock className="mt-3" text="현재 페이지 공유하기" to="share" />
