4e195742b8281c8cc1704446fdddf76299cff2a4
DongRyeong Lee <dr11291129@gmail.com>
2024-09-26 01:42:33 +0900

    feat: add PWA and icon images
    - 아이콘 이미지를 추가했습니다.
    - PWA 적용을 위해 라이브러리 설정을 완료했습니다.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,39 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import mkcert from "vite-plugin-mkcert";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), mkcert()]
+  plugins: [
+    react(),
+    svgr(),
+    mkcert(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "2024 비룡제",
+        short_name: "비룡제",
+        description: "역사를 개혁한 인하, 혁신으로 나아가다",
+        theme_color: "#1a1a1a",
+        background_color: "#1a1a1a",
+        display: "standalone",
+        start_url: ".",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
 });
